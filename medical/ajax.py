07800936837d772f073da90dc89c3bb69686840d
OmarShehata <omar.sameh.shehata@gmail.com>
2015-08-07 19:34:32 +0200

    Working on search

--- medical/ajax.py
+++ medical/ajax.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.http import Http404
 from django.http import JsonResponse
 import datetime
+from django.db.models import Q
 
 from .models import *
 import json;
@@ -11,13 +12,19 @@ def searchPatients(request):
 	#Returns patients found for that term 
 	if(not 'search' in request.POST):
 		raise Http404
-	#Search for patients by name 
-	print(request.POST['search'])
-	patientResults = Patient.objects.filter(name__icontains=request.POST['search']);
+	#Search for patients by name or phone number
+	patientResults = Patient.objects.filter(Q(name__icontains=request.POST['search']) | Q(phone=request.POST['search']));
 	patientArray = [];
 	for p in patientResults:
 		pObj = {}
+		pObj['id'] = p.pk;
 		pObj['name'] = p.name;
+		pObj['sex'] = p.sex;
+		pObj['phone'] = p.phone;
+		pObj['dob'] = {}
+		pObj['dob']['year'] = p.dateofbirth.year;
+		pObj['dob']['month'] = p.dateofbirth.month;
+		pObj['dob']['day'] = p.dateofbirth.day;
 		patientArray.append(pObj)
 
 	return JsonResponse(patientArray,safe=False)
@@ -37,5 +44,14 @@ def createPatient(request):
 		patient.sex = "female"
 	patient.phone = request.POST['phonenumber']
 	patient.save();
+
+	return HttpResponse("success");
+
+def createVisit(request):
+	#Recieves pk of patient to create a visit for 
+	#Returns "success" if all is well
+	#Returns "exists" if this patient is already registered for a visit
+	if(not 'id' in request.POST):
+		raise Http404;
 
 	return HttpResponse("success");
